6d54a4d0401732ffd3a7e4436f4f3d91d1908121
Saba Bestaev <bestaev.saba@gmail.com>
2024-04-04 17:26:00 +0300

    Добавлена форма авторизации

---
+++ ASP_Homework_Product/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using ASP_Homework_Product.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_Homework_Product.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(Login login) 
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
